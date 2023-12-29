06fc6ead3ded953c5ca6f20be5ab4235a604a356
mrtza_javidi <109103028+Mrtza-javidi@users.noreply.github.com>
2023-12-30 02:02:11 +0330

    Update practice5-blog-gallery.js

--- js/practice5-blog-gallery.js
+++ js/practice5-blog-gallery.js
@@ -1,25 +1,40 @@
 $(document).ready(function () {
-    const tooltip = $("#blog-gallery .tooltip");
-    const blogGalleryTooltipWidth = tooltip.outerWidth();
-    const blogGalleryTooltipHeight = tooltip.outerHeight();
+
+    window.addEventListener('resize', function () {
+        windowWidth = getWindowWidth();
+        if (windowWidth > 1202) {$("#blog-gallery .tooltip").css({display: "none"})}
+        if (windowWidth <= 1202) {
+            $("#blog-gallery .item").css({position: "relative"});
+            $("#blog-gallery .tooltip").css({display: "block", top: "auto !important", left: "auto !important", transform: "translateY(-80px) !important"});
+        }
+    });
+
     $("#blog-gallery .item").mousemove(function (event) {
-        var x = event.pageX;
-        var y = event.pageY;
-        // Check if tooltip exceeds window boundaries
-        if (x + blogGalleryTooltipWidth > windowWidth) {
-            x -= blogGalleryTooltipWidth;
-            tooltip.css({margin: "15px -15px"})
+        const tooltip = $("#blog-gallery .tooltip");
+        const tooltipText = $(this).find('.text');
+        const blogGalleryTextWidth = tooltipText.outerWidth(true);
+        let x = event.pageX;
+        let y = event.pageY;
+        $("body").css({overflowX: "hidden"});
+        $(this).find(tooltip).css({transform: "translateY(0)"});
+        $(this).css({position: "unset"});
+        if (x + 30 + blogGalleryTextWidth > windowWidth) {
+            x -= blogGalleryTextWidth;
+            $(this).find(tooltip).css({margin: "15px -20px"})
         } else {
-            tooltip.css({margin: "15px 20px"})
+            $(this).find(tooltip).css({margin: "15px 20px"})
         }
-        if (y + blogGalleryTooltipHeight > windowHeight) {
-            y -= blogGalleryTooltipHeight;
-        }
-        var tooltipText = $(this).find('.text').text();
-        $(this).find('.tooltip .text').text(tooltipText);
-        $(this).find('.tooltip .text').css({display: "block"});
-        tooltip.css({top: y + "px", left: x + "px"}).show();
+            $(this).find('.tooltip .text').text(tooltipText.text());
+            $(this).find('.tooltip .text').css({display: "block"});
+            $(this).find(tooltip).css({top: y + "px", left: x + "px"}).show();
+
     }).mouseout(function () {
-        $(".text").hide();
+        $(this).find('.tooltip').css({margin: "15px 20px"});
+        if (windowWidth <= 1202) {
+            $(this).find('.tooltip').css({top: "auto", left: "auto", transform: "translateY(-80px)"}).show();
+            $("#blog-gallery .item").css({position: "relative"})
+        } else {
+            $(this).find('.tooltip').hide();
+        }
     });
 });
